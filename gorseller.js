3f40b269118e261c5e7fb37feed829e62fe8cfba
ukorijeli <45639540+ukorijeli@users.noreply.github.com>
2025-07-17 17:48:11 +0300

    Update gorseller.js

--- gorseller.js
+++ gorseller.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     async function loadGorseller() {
         galleryGrid.innerHTML = '<p>Görseller yükleniyor...</p>';
         try {
-            const response = await fetch(`https://ac5e2745f52a.ngrok-free.app:5000/api/gorseller/${customerForGallery}`);
+            const response = await fetch(`https://godesign-api.onrender.com/api/gorseller/${customerForGallery}`);
             if (!response.ok) throw new Error('Görseller yüklenemedi.');
             
             const gorseller = await response.json();
